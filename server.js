974237b9a47b120b58abec4cd0f721c6860a1723
Gavin D <gavdelbo@gmail.com>
2016-05-20 10:49:59 +1000

    Created first todo routes

--- server.js
+++ server.js
@@ -3,9 +3,48 @@ var app =  express();
 var PORT = process.env.PORT || 3000;
 
 
+// Todos collection
+var todos = [{
+    id: 1,
+    description: 'Meet for lunch',
+    completed: false
+}, {
+    id: 2,
+    description: 'Go shopping',
+    completed: false
+}, {
+    id: 3,
+    description: 'Got to gym',
+    completed: false
+}];
+
+//home route
 app.get('/', function(req, res) {
     res.send('Homepage');
 });
+
+// GET /todos route
+app.get('/todos', function (req, res) {
+    res.json(todos);
+});
+// GET /todos/:id route
+app.get('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo;
+    
+    todos.forEach( function(todo) {
+        if (todoId === todo.id) {
+            matchedTodo = todo;
+        }
+    });
+    if (matchedTodo) {
+        res.json(matchedTodo);
+    } else {
+        res.status(404).send();
+    }
+});
+
+
 
 
 app.listen( PORT, function () {
