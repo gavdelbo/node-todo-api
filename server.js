e76b317dbf28f114e4c0dd0e0c980eaf614d4675
Gavin D <gavdelbo@gmail.com>
2016-05-20 13:58:29 +1000

    Create todo delete route

--- server.js
+++ server.js
@@ -51,8 +51,19 @@ app.post('/todos', function (req, res) {
    res.json(body);
    
 });
-
-
+// DELETE /todos/:id
+app.delete('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    // use underscore .findWhere function to find id to delete
+    var matchedTodo = _.findWhere(todos, {id: todoId}); 
+    
+    if (!matchedTodo) {
+        res.status(404).json({"error": "No todo found with that id"});
+    } else {
+       todos = _.without(todos, matchedTodo); 
+       res.json(matchedTodo);
+    }
+});
 app.listen( PORT, function () {
     console.log('Express is listening on port ' + PORT + '!');
 });
