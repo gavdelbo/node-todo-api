142dac13b20fe924329571428f785ea125247e07
Gavin D <gavdelbo@gmail.com>
2016-05-20 13:32:47 +1000

    Refactor and add validation to post route with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app =  express();
 var PORT = process.env.PORT || 3000;
@@ -22,13 +23,14 @@ app.get('/todos', function (req, res) {
 // GET /todos/:id route
 app.get('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo;
+    // use underscore .findWhere function to refactor
+    var matchedTodo = _.findWhere(todos, {id: todoId});
     
-    todos.forEach( function(todo) {
-        if (todoId === todo.id) {
-            matchedTodo = todo;
-        }
-    });
+    // todos.forEach( function(todo) {
+    //     if (todoId === todo.id) {
+    //         matchedTodo = todo;
+    //     }
+    // });
     if (matchedTodo) {
         res.json(matchedTodo);
     } else {
@@ -38,12 +40,14 @@ app.get('/todos/:id', function(req, res) {
 
 // POST /todos route
 app.post('/todos', function (req, res) {
-   var body = req.body; 
+   var body = _.pick(req.body, 'description', 'completed' ); 
    
+   if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ) {
+       return res.status(400).send();
+   }
+   body.description = body.description.trim();
    body.id  = todoNextId++;
-   
    todos.push(body);
-  
    res.json(body);
    
 });
