f2fd16ba5533128f0cbcb0608b3ba09e5cffa7b0
Gavin D <gavdelbo@gmail.com>
2016-05-21 16:32:41 +1000

    Change DELETE /todos/:id to sequelize

--- server.js
+++ server.js
@@ -112,15 +112,24 @@ app.post('/todos', function (req, res) {
 app.delete('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
     // use underscore .findWhere function to find id to delete
-    var matchedTodo = _.findWhere(todos, {id: todoId}); 
+   // var matchedTodo = db.todos.findById(todoId); 
     
-    if (!matchedTodo) {
-        res.status(404).json({"error": "No todo found with that id"});
-    } else {
-       todos = _.without(todos, matchedTodo); 
-       res.json(matchedTodo);
-    }
-});
+    db.todo.destroy({
+        where: {
+            id: todoId
+        }
+    }).then(function (rowsDeleted) {
+        if(rowsDeleted === 0) {
+            res.status(404).json({
+                error: 'No todo with id'
+            });
+        } else {
+            res.status(204).send();
+        }
+    }, function () {
+        res.status(500).send();
+    });
+});    
 
 // PUT /todos/:id
 app.put('/todos/:id', function (req, res) {
